0b2a3998918c0f50451699382e9ff69a2c1205f3
Mykola Korobko <korobko.nikolay@gmail.com>
2017-11-08 03:02:20 +0200

    model updated to handle KIND/CATEGORY
    but some refactoring still to be done

--- cms/models.py
+++ cms/models.py
@@ -4,14 +4,60 @@ from django.utils import timezone
 from taggit.managers import TaggableManager
 
 class Post(models.Model):
+  '''
+  TODO:
+  nice and transperent ENUM types for KIND and CATEGORY
+  CATGORY should depend on TYPE
+  should be easy to use in urls
+  '''
+  UNKNOWN = 'u'
+
+  POST = 'p'
+  FILE = 'f'
+  KINDS = (
+    (FILE, u'File'),
+    (POST, u'Post'),
+    (UNKNOWN, u'Unknown')
+  )
+  kind = models.CharField(
+    max_length=254,
+    null=False,
+    blank=False,
+    choices=KINDS,
+    default=UNKNOWN,
+  )
+
+
+  MAP = 'm'
+  PHOTO = 'p'
+  CATEGORIES =(
+    (MAP, u'Map'),
+    (PHOTO, u'Photo'),
+    (UNKNOWN, u'Unknown')
+  )
+  category = models.CharField(
+    max_length=254,
+    null=False,
+    blank=False,
+    choices=CATEGORIES,
+    default=UNKNOWN,
+  )
+
   author = models.ForeignKey('auth.User')
   title = models.CharField(max_length=200)
-  #type = models.
-  text = models.TextField()
 
+
+  text = models.TextField()
   tags = TaggableManager()
   created_date = models.DateTimeField(default=timezone.now)
   published_date = models.DateTimeField(blank=True, null=True)
+
+  def get_kind_verbose(self):
+        return dict(Post.KINDS)[self.kind]
+
+
+  def get_category_verbose(self):
+        return dict(Post.CATEGORIES)[self.category]
 
   def publish(self):
     self.published_date = timezone.now()
