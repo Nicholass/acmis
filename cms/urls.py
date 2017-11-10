d7d7420c398bd5fa87ea176432781add533b27ef
Mykola Korobko <korobko.nikolay@gmail.com>
2017-11-11 01:02:37 +0200

    post_lists now works for /(category|auhor/tag) (not really)

--- cms/urls.py
+++ cms/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.post_list, name='post_list'),
-  url(r'^author/(?P<author>\w+)/$', views.post_list, name='author_list'),
-  url(r'^category/(?P<category>\w+)/$', views.post_list, name='category_list'),
+  url(r'^author/(?P<author>\w+)/?(?P<tag>\w+)?/$', views.post_list, name='author_list'),
+  url(r'^category/(?P<category>\w+)/?(?P<tag>\w+)?/$', views.post_list, name='category_list'),
   url(r'^tag/(?P<tag>\w+)/$', views.post_list, name='tag_list'),
 
 
