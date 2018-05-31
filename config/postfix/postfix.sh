d0463ebc04cb45d92c95f55f60619692c2b56d1b
Ramenky Underground <ramenky27@gmail.com>
2018-05-31 23:46:48 +0300

    Add opendkim to postfix image

--- config/postfix/postfix.sh
+++ config/postfix/postfix.sh
@@ -4,6 +4,12 @@
 echo "virtual_alias_domains = "$DOMAINS >> /etc/postfix/main.cf
 echo "virtual_alias_maps = hash:/etc/postfix/virtual" >> /etc/postfix/main.cf
 
+#add opendkim support
+echo "milter_protocol = 2" >> /etc/postfix/main.cf
+echo "milter_default_action = accept" >> /etc/postfix/main.cf
+echo "smtpd_milters = inet:localhost:12301" >> /etc/postfix/main.cf
+echo "non_smtpd_milters = inet:localhost:12301" >> /etc/postfix/main.cf
+
 # add docker network
 sed -i 's/^mynetworks.*/mynetworks = 172.0.0.0\/8/' /etc/postfix/main.cf
 
@@ -12,4 +18,5 @@ sed -i 's/^mynetworks.*/mynetworks = 172.0.0.0\/8/' /etc/postfix/main.cf
 postmap /etc/postfix/virtual
 service postfix reload
 service postfix restart
+service opendkim restart
 tail -f /var/log/mail.log
