9dcd9fc91b877eb7267f54ea8d5b8ea429f34659
Ramil Nazarov <ramenky27@gmail.com>
2018-07-11 15:31:14 +0300

    Removed temporary translation files, Quickfix cmscategory for deploy, Quickfix avatars in comments

--- acis/settings.py
+++ acis/settings.py
@@ -71,6 +71,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sites.middleware.CurrentSiteMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
