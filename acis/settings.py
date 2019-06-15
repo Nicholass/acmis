0c2ccd44e2a657c87bfe948df2d3c2e0a5e6b71f
Ramenky Underground <ramenky27@gmail.com>
2019-06-15 15:25:18 +0300

    Fix l10n and i18n settings

--- acis/settings.py
+++ acis/settings.py
@@ -144,14 +144,14 @@ AUTHENTICATION_BACKENDS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'uk'
+LANGUAGE_CODE = 'uk-UK'
 LANGUAGES = (
     ('uk', 'Ukrainian'),
 )
 
 TIME_ZONE = 'Europe/Kiev'
 
-USE_I18N = False
+USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
@@ -166,7 +166,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'djangobower.finders.BowerFinder',
 )
 
 FILE_UPLOAD_PERMISSIONS = 0o755
