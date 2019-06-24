208826f3c40b52653ba42eb746fb2a7c4d4fb47a
Ramenky Underground <ramenky27@gmail.com>
2019-06-25 01:51:33 +0300

    Add font blinking for new PM button

--- cms/static/js/misc.js
+++ cms/static/js/misc.js
@@ -8,4 +8,22 @@ $(document).ready(function(){
         $('.comment').removeClass('selected');
         $(event.target.hash).addClass('selected');
     });
+
+    // Blink PM button if has new
+    var blink = $('.blink');
+    if (blink) {
+        blink.css('font-weight', 'bold');
+        setInterval(function () {
+            blink.each(function () {
+                var element = $(this);
+                if (element.css('font-weight') === '700') {
+                    element.css('color', '#666');
+                    element.css('font-weight', 'normal');
+                } else {
+                    element.css('color', '#FFF');
+                    element.css('font-weight', 'bold');
+                }
+            });
+        }, 200);
+    }
 });
