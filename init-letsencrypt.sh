9e62fe343a9f6ac95db67e72d09051863aeb9ee0
Ramenky Underground <ramenky27@gmail.com>
2019-12-21 16:20:36 +0200

    Fix init-certbot.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,9 +7,9 @@ fi
 
 domains=(diggers.kiev.ua www.diggers.kiev.ua)
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="./certbot"
 email="ramenky27@gmail.com" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -29,6 +29,7 @@ fi
 
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
+mkdir -p "$data_path/conf/live"
 mkdir -p "$data_path/conf/live/$domains"
 docker-compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:1024 -days 1\
