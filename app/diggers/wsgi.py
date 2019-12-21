b24445e6fd71820d9665ed4881aee0c832a93b42
Ramenky Underground <ramenky27@gmail.com>
2019-12-21 04:24:46 +0200

    Replace acis project name to diggers

--- app/diggers/wsgi.py
+++ app/diggers/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for acis project.
+WSGI config for diggers project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "acis.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "diggers.settings")
 
 application = get_wsgi_application()
