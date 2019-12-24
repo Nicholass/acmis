0e3157668cd5c0303e66a6e52218e8b9eab362aa
Ramenky Underground <ramenky27@gmail.com>
2019-12-24 23:25:27 +0200

    Add tiny long url

--- app/cms/pipeline.py
+++ app/cms/pipeline.py
@@ -1,13 +1,22 @@
+from __future__ import with_statement
 import os
 import wget
+import contextlib
 from uuid import uuid4
 from django.conf import settings
 from django.contrib.auth.models import User
+from urllib.request import urlopen
+from urllib.parse import urlencode
 
 def download_avatar(url):
     file = '{}.{}'.format(uuid4().hex, 'jpg')
     wget.download(url, os.path.join(getattr(settings, 'MEDIA_ROOT'), file))
     return file
+
+def make_tiny(url):
+    request_url = ('https://tinyurl.com/api-create.php?' + urlencode({'url':url}))
+    with contextlib.closing(urlopen(request_url)) as response:
+        return response.read().decode('utf-8 ')
 
 def load_user(**kwargs):
     try:
@@ -34,8 +43,9 @@ def save_profile(backend, user, response, *args, **kwargs):
         if avatar and not profile.avatar:
             profile.avatar = download_avatar(avatar['data']['url'])
 
-        if not profile.facebook:
-            profile.facebook = response.get('link')
+        link = response.get('link')
+        if link and not profile.facebook:
+            profile.facebook = make_tiny(link)
 
         gender = response.get('gender')
         if gender and not profile.gender:
