c36dd52cd71843c07eb6a2f02c4291f0d5cf3475
Ramenky Underground <ramenky27@gmail.com>
2019-12-24 23:18:53 +0200

    Fix profile avatar upload for facebook

--- app/cms/pipeline.py
+++ app/cms/pipeline.py
@@ -5,8 +5,7 @@ from django.conf import settings
 from django.contrib.auth.models import User
 
 def download_avatar(url):
-    filename, ext = (url.split('/')[-1].split('.'))
-    file = '{}.{}'.format(uuid4().hex, ext)
+    file = '{}.{}'.format(uuid4().hex, 'jpg')
     wget.download(url, os.path.join(getattr(settings, 'MEDIA_ROOT'), file))
     return file
 
@@ -32,7 +31,7 @@ def save_profile(backend, user, response, *args, **kwargs):
 
     if backend.name == 'facebook':
         avatar = response.get('picture')
-        if avatar['data'] and avatar['data']['url'] and not profile.avatar:
+        if avatar and not profile.avatar:
             profile.avatar = download_avatar(avatar['data']['url'])
 
         if not profile.facebook:
