4b983b7aa0a466c20263b47b90afd35cdf8adbcb
Ramenky Underground <ramenky27@gmail.com>
2020-05-03 20:38:05 +0300

    Improve markdown in comments

--- app/cms/templatetags/markdown_extras.py
+++ app/cms/templatetags/markdown_extras.py
@@ -9,4 +9,7 @@ register = template.Library()
 @register.filter()
 @stringfilter
 def markdown(value):
-    return md.markdown(value, extensions=['markdown.extensions.fenced_code'])
+    return md.markdown(value, extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.nl2br',
+    ])
